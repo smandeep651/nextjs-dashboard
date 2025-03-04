63b6601d798e22ec55d993b366fbfdd3927c1a96
Mandeep Singh <smandeep651@gmail.com>
2025-03-03 19:19:29 -0500

    chapter16finished

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -2,6 +2,9 @@
 import { z } from 'zod';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
+ 
 
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
@@ -105,4 +108,24 @@ const FormSchema = z.object({
    
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
+  }
+
+  
+  export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData,
+  ) {
+    try {
+      await signIn('credentials', formData);
+    } catch (error) {
+      if (error instanceof AuthError) {
+        switch (error.type) {
+          case 'CredentialsSignin':
+            return 'Invalid credentials.';
+          default:
+            return 'Something went wrong.';
+        }
+      }
+      throw error;
+    }
   }
